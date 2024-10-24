354363bee8e3f67cf303bcb48fa490e75ed4ce71
Enzo949494 <9812@holbertonstudents.com>
2024-10-24 09:09:42 +0000

    putchar

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,19 +10,16 @@
 
 int main(void)
 {
-int i, j;
-for (i = 0; i < 10; i++)
+int d = '0';
+while (d <= '9')
 {
-j = i;
-while (j < 10)
-{
-putchar(j + '0');
-if (i != 8 || j != 9)
+putchar(d);
+if (d != '9')
 {
 putchar(',');
 putchar(' ');
 }
-j++;
+d++;
 }
 }
 putchar('\n');
