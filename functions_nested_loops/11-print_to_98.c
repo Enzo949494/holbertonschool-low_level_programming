c1f2ef333bade2c20f2ad17cb5b2718788333642
Enzo949494 <9812@holbertonstudents.com>
2024-10-25 09:47:52 +0000

    98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,8 @@
+#include "main.h"
+
 /**
-* print_to_98 - prints all natural numbers from n to 98
-* @n: starting number
+* print_to_98 - imprime nombre naturel de n a 98
+* @n: nombre depart
 */
 void print_to_98(int n)
 {
