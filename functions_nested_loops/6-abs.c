8267c109d6a1045a657e29412568f6ab3c8e9106
Enzo949494 <9812@holbertonstudents.com>
2024-10-25 08:26:51 +0000

    _abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _abs - calcule valeur d1 entier
+* @n: valeur a contoler
+*
+* Return: valeur absolu de n
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+return (-n);
+}
