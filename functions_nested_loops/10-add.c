1e3a1a2ac2878e08283b96c043751e02444c2271
Enzo949494 <9812@holbertonstudents.com>
2024-10-25 09:22:51 +0000

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - additione 2 entree et resultat
+* @a: entree a additionner a b
+* @b: entree a additionner a a
+* Return: somme a et b
+*/
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
