f0e7a8bab8f27f0406e0bd6a07d1c93d2156ab98
Enzo949494 <9812@holbertonstudents.com>
2024-10-28 11:35:52 +0000

    affiche nombre de 0 a 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - affiche nombre de 0 a 9,saut de ligne
+*/
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
