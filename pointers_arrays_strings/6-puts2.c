6ef68d6fc15346635f16275b31848a06d38133b1
Enzo949494 <9812@holbertonstudents.com>
2024-10-30 13:17:42 +0000

    imprime tout les autres caractere

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* puts2 - Prints every other character of a string
+* @str: The string to be printed
+*/
+void puts2(char *str)
+{
+for (int i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
