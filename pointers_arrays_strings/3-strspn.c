c31fe9353fb4bcafaf3b01f4932e26dfe2957a30
Enzo949494 <9812@holbertonstudents.com>
2024-11-04 21:50:45 +0100

    nombre de cara dans une sous chaine

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - donne longueur prefixe sous chaine
+ * @s: chaine a scanner
+ * @accept: chaine avec cara a verif
+ * Return: resultat 
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int c = 0;
+    int f;
+    char *a;
+    while (*s)
+    {
+        f = 0;
+        for (a = accept; *a; a++)
+        {
+            if (*s == *a)
+            {
+                f = 1;
+                break;
+            }
+        }
+        if (f == 0)
+        return c;
+        c++;
+        s++;
+    }
+    return c;
+}
