fce9d4ab492e4d8877faba5242c7e1ce8b7d981a
Enzo949494 <9812@holbertonstudents.com>
2024-11-04 14:23:45 +0100

    remplir une zone avec octet

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * 
- */
+ * _memset - remplit memoire avec octet constant
+ * @s: pointeur vers zone memoire a remplir
+ * @b: octet constant a placer
+ * @n: nombre octet a remplir
+ * Return: pointeur vers zone memoire s
+ * /
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return s;
+}
