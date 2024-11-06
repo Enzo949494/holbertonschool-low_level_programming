844caa11dbf8498384391d9c253818769893af50
Enzo949494 <9812@holbertonstudents.com>
2024-11-06 14:32:06 +0100

    calcul valeur elevee

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow-recursion - retourne valeur x eleve puissance y
+ * @x: nombre de base
+ * @y: exposant
+ * Return: resultat de x^y, ou -1 si y negatif
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return -1;
+	}
+	if  (y == 0)
+	{
+		return  1;
+	}
+	return x * _pow_recursion(x, y -1);
+}
