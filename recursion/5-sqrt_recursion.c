9f3bcd1510c3574c16f49813867f30c29fac541b
Enzo949494 <9812@holbertonstudents.com>
2024-11-07 13:34:04 +0100

    sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * _sqrt_helper - calcule racine carre recursivement 
+ * _sqrt_helper - calcule racine carre recursivement
  * @n: nombre a calculer racine carre
  * @i: estimation actuelle
- * 
+ *
  * Return: racine carre de n, -1 si existe pas
  */
 
 int _sqrt_helper(int n, int i)
 {
-	if (i * i ==n)
-	return i;
+	if (i * i == n)
+	return (i);
 	if (i * i > n)
-	return -1;
-	return _sqrt_helper(n , i +1);
+	return (-1);
+	return (_sqrt_helper(n, i + 1));
 }
 
 /**
@@ -27,6 +27,6 @@ int _sqrt_helper(int n, int i)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	return -1;
-	return _sqrt_helper(n, 0);
+	return (-1);
+	return (_sqrt_helper(n, 0));
 }
