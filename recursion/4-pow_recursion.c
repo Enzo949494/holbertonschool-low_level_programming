dac0ff18d573a24ef897bf2bab94dc0e3a93ba2a
Enzo949494 <9812@holbertonstudents.com>
2024-11-07 13:31:05 +0100

    pow recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _pow-recursion - retourne valeur x eleve puissance y
+ * _pow_recursion - retourne valeur x eleve puissance y
  * @x: nombre de base
  * @y: exposant
  * Return: resultat de x^y, ou -1 si y negatif
@@ -12,11 +12,11 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	if  (y == 0)
 	{
-		return  1;
+		return  (1);
 	}
-	return x * _pow_recursion(x, y -1);
+	return (x * _pow_recursion(x, y - 1));
 }
