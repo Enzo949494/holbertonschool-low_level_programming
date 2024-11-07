89ea7eb9e8b77379085ddfdb4fa56308ef8fc05b
Enzo949494 <9812@holbertonstudents.com>
2024-11-07 11:21:58 +0100

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,21 +12,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
-    if (*needle == '\0')
-    return (haystack);
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        if (haystack[i] == needle[0])
-        {
-            for (j = 0; needle[j] != '\0'; j++)
-            {
-                if (haystack[i + j] != needle[j])
-                break;
-            }
-            if (needle[j] == '\0')
-            return (haystack + i);
-        }
-    }
-    return (NULL);
+	int i, j;
+
+	if (*needle == '\0')
+	return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				break;
+			}
+			if (needle[j] == '\0')
+			return (haystack + i);
+		}
+	}
+	return (NULL);
 }
