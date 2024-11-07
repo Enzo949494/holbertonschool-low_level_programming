9aa492f7d5ee5585dc9c84a1fc0f046f1007bf4e
Enzo949494 <9812@holbertonstudents.com>
2024-11-07 13:40:16 +0100

    prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -12,22 +12,23 @@ int is_prime_helper(int n, int i);
 int is_prime_number(int n)
 {
 	if (n <= 1)
-	return 0;
-    return is_prime_helper(n, 2);
+	return (0);
+	return (is_prime_helper(n, 2));
 }
 
 /**
- * is_prime_helper - verfie si nombre premeir
+ * is_prime_helper - verfie si nombre premier
  * @n: nombre a verif
  * @i: diviseur a verifier
- * return: 1 si premier, sinon 0
+ *
+ * Return: 1 si premier, sinon 0
  */
 
 int is_prime_helper(int n, int i)
 {
 	if (i * i > n)
-	return 1;
+	return (1);
 	if (n % i == 0)
-	return 0;
-	return is_prime_helper(n, i + 1);
+	return (0);
+	return (is_prime_helper(n, i + 1));
 }
