3d96286264528ec115a95233b38f05dcc36b67a2
Enzo949494 <9812@holbertonstudents.com>
2024-11-07 15:03:18 +0100

    imprime

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - imprime nom programme
+ * @argc: nombre arguments ds ligne commande
+ * @argv: tableau arguments commandes
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
