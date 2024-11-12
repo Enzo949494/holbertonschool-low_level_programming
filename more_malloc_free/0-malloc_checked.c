ca5bd37726e7a0a8eab906255b79f7317d4c3d5d
Enzo949494 <9812@holbertonstudents.com>
2024-11-12 15:26:24 +0100

    allouer memoire avec malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - alloue memoire avec malloc
+ * @b: nombre octet a allouer
+ * Return: pointeur vers memoire allouer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
