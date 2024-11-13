3ba47e83684d8747ed9614805ef06222081660d3
Enzo949494 <9812@holbertonstudents.com>
2024-11-13 10:31:23 +0100

    creer tableau

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creer tableau d entier
+ * @min: valeur mini
+ * @max: valeur max
+ * Return: pointeur vers new tabl,NULL si echec
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int size, i;
+
+	if (min > max)
+	return (NULL);
+
+	size = max - min + 1;
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+	return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = min + i;
+	return (arr);
+}
