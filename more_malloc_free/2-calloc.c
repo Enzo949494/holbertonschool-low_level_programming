ebf24db2bf94bc5a99c0273270e44458ec211276
Enzo949494 <9812@holbertonstudents.com>
2024-11-13 10:19:23 +0100

    alloue mem a tableau

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,4 +3,28 @@
 
 /**
  * _calloc - alloue memoire pour tableau
+ * @nmemb: nombre elements
+ * @size: taille elements
+ * Return: pointeur vers memoire alluer, NULL si echec
  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_size;
+	unsigned char *byte_ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+	return (NULL);
+
+	byte_ptr = (unsigned char *)ptr;
+	for (i = 0; i < total_size; i++)
+		byte_ptr[i] = 0;
+	return (ptr);
+}
