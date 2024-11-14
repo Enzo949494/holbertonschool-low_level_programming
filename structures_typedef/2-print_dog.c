9a845365e4818746f2f1321990ea4192a9767da6
Enzo949494 <9812@holbertonstudents.com>
2024-11-14 17:56:36 +0100

    affiche infos chien

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - imprime info struct dog
+ * @d: pointeur vers struct dog a imprimer
+ * 
+ * Description: fonction imprime infos chien
+ * si elements NULL, imprime nil
+ * si d est NULL, imprime rien
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+}
