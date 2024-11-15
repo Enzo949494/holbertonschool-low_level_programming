9f1e20f90f36266a8770c0c39222f355ee3f5880
Enzo949494 <9812@holbertonstudents.com>
2024-11-15 10:09:49 +0100

    new dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,14 +9,12 @@
  * Description: structure avec infos sur chien
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+} dog_t;
 
 /* proto fonctions structure dog*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
