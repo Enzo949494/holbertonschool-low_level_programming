4b0a48536cde389b5d9cc9e059e82a98db4bc371
Enzo949494 <9812@holbertonstudents.com>
2024-11-15 09:52:10 +0100

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,33 +13,48 @@
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
+
 {
-	dog_t *d;
+    dog_t *d;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
+    d = malloc(sizeof(dog_t));
+    if (d == NULL)
+        return (NULL);
 
-	d->name = (name != NULL) ? malloc(strlen(name) + 1) : NULL;
-	if (name != NULL && d->name == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
-	if (name != NULL)
-		strcpy(d->name, name);
+    
+    if (name != NULL)
+    {
+        d->name = malloc(strlen(name) + 1);
+        if (d->name == NULL)
+        {
+            free(d);
+            return (NULL);
+        }
+        strcpy(d->name, name);
+    }
+    else
+    {
+        d->name = NULL;
+    }
 
-	d->owner = (owner != NULL) ? malloc(strlen(owner) + 1) : NULL;
-	if (owner != NULL && d->owner == NULL)
-	{
-		free(d->name);
-		free(d);
-		return (NULL);
-	}
-	if (owner != NULL)
-		strcpy(d->owner, owner);
+    
+    if (owner != NULL)
+    {
+        d->owner = malloc(strlen(owner) + 1);
+        if (d->owner == NULL)
+        {
+            free(d->name);
+            free(d);
+            return (NULL);
+        }
+        strcpy(d->owner, owner);
+    }
+    else
+    {
+        d->owner = NULL;
+    }
 
-	d->age = age;
+    d->age = age;
 
-	return (d);
+    return (d);
 }
