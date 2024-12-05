8cdd804288888bc63a53eb3b2aeac848dc843f75
Enzo949494 <9812@holbertonstudents.com>
2024-12-05 18:39:41 +0100

    main-copy,few fonctions for copy file to other file

--- file_io/main.h
+++ file_io/main.h
@@ -6,15 +6,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int close_file_free_buff(int file_descriptor, char *buff, int returnal);
+int close_file_no_free(int file_descriptor, int returnal);
+int main_copy(int argc, char *argv[]);
 
 #define BUFFER_SIZE 1024
-
-int close_file_free_buff(int file_descriptor, char *buff, int returnal);
-
-int close_file_no_free(int file_descriptor, int returnal);
 
 #endif /* MAIN_H */
